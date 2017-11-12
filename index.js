8bdfe55a9fe44bf8c44f453923a8232e770d02af
Peter Coyle <coyle.peters@gmail.com>
2017-11-12 20:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function currentLine() {
     for(var i = 0; i < katzDeliLine.length; i++){
       if(i === katzDeliLine.length-1){
         console.log(output + (i+1).toString() + ". " + katzDeliLine[i])
-        return output + (i+1).toString() + ". " + katzDeliLine[i]
+        return output + (i+1).toString() + '. ' + katzDeliLine[i]
       }
-      output += (i+1).toString() + + ". " + katzDeliLine[i] + ","
+      output += (i+1).toString() + '. ' + katzDeliLine[i] + ","
     }
   } else {
     return "The line is currently empty."
