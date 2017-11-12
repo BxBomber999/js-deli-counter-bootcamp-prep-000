3b40e708feaeaaf483d1b1ac9778d7252c3f2128
Peter Coyle <coyle.peters@gmail.com>
2017-11-12 20:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ function currentLine() {
   }
 }
 
-takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
-currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
-
-nowServing(katzDeliLine); // "Currently serving Ada."
-
-console.log(currentLine(katzDeliLine));
+console.log(takeANumber(katzDeliLine, "Ada")); // "Welcome, Ada. You are number 1 in line."
+// console.log(takeANumber(katzDeliLine, "Grace")); // "Welcome, Grace. You are number 2 in line."
+// console.log(takeANumber(katzDeliLine, "Kent")); // "Welcome, Kent. You are number 3 in line."
+// console.log(currentLine(katzDeliLine)); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+// 
+// console.log(nowServing(katzDeliLine)); // "Currently serving Ada."
+// 
+// console.log(currentLine(katzDeliLine));
